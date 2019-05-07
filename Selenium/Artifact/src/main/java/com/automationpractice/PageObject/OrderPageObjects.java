d347b375e67a85c9661c2a8136930eff64fa1831
Kranti Biswakarma <krantibiswakarma@Krantis-MacBook-Pro.local>
2019-05-07 00:09:39 -0400

    added maven compiler plugin to pom file

--- Selenium/Artifact/src/main/java/com/automationpractice/PageObject/OrderPageObjects.java
+++ Selenium/Artifact/src/main/java/com/automationpractice/PageObject/OrderPageObjects.java
@@ -5,12 +5,9 @@ import static org.hamcrest.Matchers.equalToIgnoringCase;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Base.BaseUtil;
 
